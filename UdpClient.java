c1245cf4263eebd7b7fbf90a0d4dc4ebc9db5df5
Alfredo G Ceballos <agceballos@cpp.edu>
2017-11-14 08:12:37 -0800

    Done but needs cleanup

--- UdpClient.java
+++ UdpClient.java
@@ -47,11 +47,10 @@ public class UdpClient {
         short length;
         length = (short) (data.length + header.length);
         
-        header[2] = (byte) ((length & 0xFF00) >> 8);
-        header[3] = (byte) (length & 0x00FF);   // Length
-        
-        header[4] = 0;
-        header[5] = 0;  // Identification   
+        header[2] = (byte) ((length & 0xFF00) >> 8); // Length
+        header[3] = (byte) (length & 0x00FF);
+        header[4] = 0;  // Identification 
+        header[5] = 0;
         header[6] = 64; // 01000000 : Flags 010, Offset 00000
         header[7] = 0;  // Offset
         header[8] = 50; // TTL : 50 seconds
@@ -82,7 +81,7 @@ public class UdpClient {
         header[11] = (byte) (cks & 0x00FF);
         
         
-        // The packet's header data is already in the header byte array, here
+        // The packet's header data is in the header byte array, here
         // it is copied into the packet byte array, which is the size of 
         // of the header plus the size of the data generated, that data is then
         // also copied in at the end
@@ -98,12 +97,16 @@ public class UdpClient {
     // byte values, then inserting them into the packet
     public static byte[] getActualIP() {
         byte[] ipAddress = new byte[4];
-        URL getIP = new URL("http://checkip.amazonaws.com");
-        BufferedReader in = new BufferedReader(new InputStreamReader(getIP.openStream(), "UTF-8"));
-        String[] src = in.readLine().split("\\.");
-        for (int i = 0; i < src.length; i++) {
-            int j = Integer.parseInt(src[i]);
-            ipAddress[i] = (byte) j;  // source address bytes
+        try {
+            URL getIP = new URL("http://checkip.amazonaws.com");
+            BufferedReader in = new BufferedReader(new InputStreamReader(getIP.openStream(), "UTF-8"));
+            String[] src = in.readLine().split("\\.");
+            for (int i = 0; i < src.length; i++) {
+                int j = Integer.parseInt(src[i]);
+                ipAddress[i] = (byte) j;  // source address bytes
+            }
+        }catch(Exception e) {
+            System.out.println("It broke...");
         }
         return ipAddress;
     }
@@ -115,7 +118,7 @@ public class UdpClient {
     public static byte[] genByteArray(int n) {
         int numBytes = (int)Math.pow(2, n);
         byte[] arr = new byte[numBytes];
-        System.out.println("size of data " + arr.length);
+        //System.out.println("size of data " + arr.length);
         
         for(int i = 0; i  < numBytes; i++){
             arr[i] = (byte)(Math.random() * 255);
@@ -124,29 +127,33 @@ public class UdpClient {
         return arr;
     }
     
-    /*
-     * This method handles creating a UPD packet
-     *
-     */
-    public static byte genUDP(int destPort, int dataSize) {
-        byte[] udpPacket = new byte[8 + dataSize];
+    // Generates UDP packet
+    public static byte[] genUDP(int destPort, int dataSize) {
         byte[] randData = genByteArray(dataSize);
         int dataLength = randData.length;
+        byte[] udpPacket = new byte[8 + dataLength];
         
         udpPacket[0] = 0; // Source port
-        updPacket[1] = 0;
+        udpPacket[1] = 0;
         
-        updPacket[2] = (byte)(destPort >> 8); // Destination port
-        updPacket[3] = (byte)(destPort);
+        udpPacket[2] = (byte)(destPort >> 8); // Destination port
+        udpPacket[3] = (byte)(destPort);
         
         udpPacket[4] = (byte)(dataLength >> 8); // Length of data
-        udpPacket[5] = (byte)(dataLength);
+        udpPacket[5] = (byte) dataLength;
+        // elements 6 and 7 hold the checksum, which needs a psuedo header
+        // to be calculated, elements 8 and on contain the data
+        for(int i = 0; i < dataLength; i++) {
+            udpPacket[i + 8] = randData[i];
+        }
+        
         
         byte[] pHeader = new byte[12]; // Psuedo-header
-        byte[] temp = getActualIP();
+        byte[] temp = getActualIP(); // Getting source address
         for(int i = 0; i < temp.length; i++) {
-            pHeader[i] = temp[i];
+            pHeader[i] = temp[i]; // Sticking source address to pseudo header
         }
+        
         pHeader[4] = (byte)18; //Destination address
         pHeader[5] = (byte)221;
         pHeader[6] = (byte)102;
@@ -158,18 +165,33 @@ public class UdpClient {
         pHeader[10] = (byte)(dataLength >> 8); // Data length
         pHeader[11] = (byte)dataLength;
         
-        
+        // The header that gets sent to the checksum is made up of the
+        // udp packet and the pseudo header
+        int tempSize = 8 + 12 + dataLength;
+        byte[] checksumPacket = new byte[tempSize];
+        int j = 0;
+        for (int i = 0; i < tempSize; i++) {
+            if (i < (8 + dataLength)) {
+                checksumPacket[i] = udpPacket[i];
+            } else {
+                checksumPacket[i] = pHeader[j];
+                j++;
+            }
         }
+        short checksum = checksum(checksumPacket);
+        udpPacket[6] = (byte) ((checksum >> 8) & 0xFF);
+        udpPacket[7] = (byte) (checksum & 0xFF);
+        System.out.println("UDP packet size is " + udpPacket.length);
+        return udpPacket;
+        } // End of genUDP
     
     public static void main(String[] args) throws Exception {
         try (Socket socket = new Socket("18.221.102.182", 38005)) {
             // Creating client input/output streams to receive 
             // and send messages from and to server 
             InputStream is = socket.getInputStream();
-            InputStreamReader isr = new InputStreamReader(is, "UTF-8");
-            BufferedReader br = new BufferedReader(isr);
+            InputStreamReader isr = new InputStreamReader(is);
             OutputStream os = socket.getOutputStream();
-            BufferedInputStream bis = new BufferedInputStream();
             
             // "Handshaking" process, hard-coding 0xDEADBEEF
             byte[] b  = {(byte)0xDE, (byte)0xAD, (byte)0xBE,(byte)0xEF};
@@ -181,12 +203,31 @@ public class UdpClient {
             }
             int portNum = (is.read() << 8);
             portNum += is.read();
-            System.out.printf("\nPort number received: %d", portNum);
+            System.out.printf("%nPort number received: %d%n", portNum);
             
-            for(int i = 0; i < 12; i++) {
+            float totalRTT = 0;
+            for(int i = 1; i <= 12; i++) {
+                long sent, received, rtt;
+                String response = "";
+                byte[] udpPacket = genUDP(portNum, i);
+                byte[] iPV4Packet = IPv4Packet(udpPacket);
                 
+                System.out.printf("Sending packet with %d byte of data%n", (int)Math.pow(2, i));
+                sent = System.currentTimeMillis(); // Data about to be sent                
+                os.write(iPV4Packet);
+                for(int j =  0; j < 4; j++){
+                    response += Integer.toHexString(is.read()).toUpperCase();
+                }
+                // Data received and processed
+                received = System.currentTimeMillis();
+                System.out.println("0x" + response);
+                rtt = received - sent;
+                System.out.println("RTT: " + rtt);
+                totalRTT =+ rtt;
             }
-        }
+            
+            System.out.println("Average RTT: " + (totalRTT / 12));
+        } // End of try
     }// End of main
     
 }// End of file
